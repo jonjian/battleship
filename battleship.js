89d3d941516dcf8f2796e08b485ceb45e6af4128
jonjian <jonjian@gmail.com>
2018-08-09 19:17:16 -0700

    basic game functionality done, ships can be placed/hit/sunk and players can win

--- battleship.js
+++ battleship.js
@@ -1,4 +1,9 @@
 const model = {
+  /* 0 = empty
+     1 = ship
+     2 = hit
+     3 = miss
+  */
   player1Board: [0,0,0,0,0,
                   0,0,0,0,0,
                   0,0,0,0,0,
@@ -11,6 +16,8 @@ const model = {
                   0,0,0,0,0],
   player1Ships: 0,
   player2Ships: 0,
+  player1LoadedShips: false,
+  player2LoadedShips: false,
   player1Keys: [],
   player2Keys: [],
 }
@@ -28,10 +35,106 @@ const view = {
 
 const controller = {
   player1BoardKeyClick: num => {
-    console.log(num)
+    // fire
+    if (model.player1LoadedShips) {
+      switch (model.player1Board[num]) {
+        case 0:
+          model.player1Board[num] = 3
+          view.displayMessage('Miss!')
+          break;
+        case 1:
+          model.player1Board[num] = 2
+          view.displayMessage('Hit!')
+          model.player1Ships -= 1
+          if (model.player1Ships > 0) {
+            if (model.player1Ships > 1) {
+              view.displayMessage(`You have Sunk a Ship! Player 1 has ${model.player2Ships} ships left!`)
+            } else {
+              view.displayMessage(`Their forces grow weak, Player 1 has 1 ship left!`)
+            }
+          } else {
+            alert(`Congratulations Player 2! You won!`)
+          }
+          break;
+        case 2:
+          alert('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          break;
+        case 3:
+          alert('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          break;
+        default:
+          console.log('error unknown case: battleship.js line 62')
+      }
+    }
+    //loading ships
+    if (!model.player1LoadedShips && model.player1Ships < 4) {
+      if (model.player1Board[num] === 0) {
+        model.player1Board[num] = 1
+        model.player1Ships += 1
+        if (model.player1Ships > 1) {
+          view.displayMessage(`${model.player1Ships} Ships Placed!`)
+        } else {
+          view.displayMessage(`1 Ship Placed!`)
+        }
+      } else {
+        alert('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
+      }
+      if (model.player1Ships === 3) {
+        model.player1LoadedShips = true
+      }
+    }
+    console.log('click on p1 tile: ',num)
   },
   player2BoardKeyClick: num => {
-    console.log(num)
+    //fire
+    if (model.player2LoadedShips) {
+      switch (model.player2Board[num]) {
+        case 0:
+          model.player2Board[num] = 3
+          view.displayMessage('Miss!')
+          break;
+        case 1:
+          model.player2Board[num] = 2
+          view.displayMessage('Hit!')
+          model.player2Ships -= 1
+          if (model.player2Ships > 0) {
+            if (model.player1Ships > 1) {
+              view.displayMessage(`You have Sunk a Ship! Player 2 has ${model.player2Ships} ships left!`)
+            } else {
+              view.displayMessage(`Their forces grow weak, Player 2 has 1 ship left!`)
+            }
+          } else {
+            alert(`Congratulations Player 1! You won!`)
+          }
+          break;
+        case 2:
+          alert('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          break;
+        case 3:
+          alert('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          break;
+        default:
+          console.log('error unknown case: battleship.js line 62')
+      }
+    }
+    //loading ships
+    if (!model.player2LoadedShips && model.player2Ships < 4) {
+      if (model.player2Board[num] === 0) {
+        model.player2Board[num] = 1
+        model.player2Ships += 1
+        if (model.player2Ships > 1) {
+          view.displayMessage(`${model.player2Ships} Ships Placed!`)
+        } else {
+          view.displayMessage(`1 Ship Placed!`)
+        }
+      } else {
+        alert('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
+      }
+      if (model.player2Ships === 3) {
+        model.player2LoadedShips = true
+      }
+    }
+    console.log('click on p2 tile: ', num)
   }
 }
 
@@ -47,8 +150,8 @@ const bindBoardClickToKeys = () => {
 }
 
 init = () => {
-  view.displayMessage('Player 1 - Place your ships')
   bindBoardClickToKeys()
+  view.displayMessage('Hello! Please read instructions..')
   console.log('finish init render')
 }
 
