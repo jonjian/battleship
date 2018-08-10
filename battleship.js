556ad76af458eb2040aa8ff17295d599551838a6
jonjian <jonjian@gmail.com>
2018-08-09 18:04:16 -0700

    added bindBoardClickToKeys method, board clicks now register

--- battleship.js
+++ battleship.js
@@ -1,4 +1,4 @@
-var model = {
+const model = {
   player1Board: [0,0,0,0,0,
                   0,0,0,0,0,
                   0,0,0,0,0,
@@ -11,19 +11,48 @@ var model = {
                   0,0,0,0,0],
   player1Ships: 0,
   player2Ships: 0,
-  
+  player1Keys: [],
+  player2Keys: [],
 }
 
-var view = {
+const view = {
   displayMessage: msg => {
     const node = document.createElement("LI")
-    const textnode = document.createTextNode(msg)
+    const time = new Date()
+    const timeStamp = time.toLocaleTimeString('en-US')
+    const textnode = document.createTextNode(`${timeStamp}: ${msg}`)
     node.appendChild(textnode)
     document.getElementById("messages").appendChild(node)
   },
-  
 }
 
-var controller = {
+const controller = {
+  boardKeyClick: () => {
+    console.log('click')
+  }
+}
+
+const bindBoardClickToKeys = () => {
+  let num1 = 0
+  let num2 = 25
+  //player1
+  for (let i = 0; i < 25; i += 1) {
+    //player 1
+    let key = document.getElementById(`player1_cell_${num1}`)
+    model.player1Keys.push(key.addEventListener("click", () => controller.boardKeyClick(num1.toString())))
+    num1 += 1
+    //player 2
+    key = document.getElementById(`player2_cell_${num2.toString()}`)
+    model.player2Keys.push(key.addEventListener("click", () => controller.boardKeyClick(num2.toString())))
+    num2 += 1
+  }
 
 }
+
+init = () => {
+  view.displayMessage('Player 1 - Place your ships')
+  bindBoardClickToKeys()
+  console.log('finish init render')
+}
+
+window.onload = init
