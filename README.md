d8b23e1f4f4e6a2c3298b704fc2839097099049f
jonjian <jonjian@gmail.com>
2018-08-10 17:10:03 -0700

    added readme info and JS testing suite

---
+++ README.md
@@ -0,0 +1,28 @@
+# mini Battleship
+
+## Getting STarted
+
+Open HTML file in browser
+
+## To run end to end testing: 
+
+### Enter the following into the browser developer console
+
+```
+runTestingSuite()
+```
+
+### Notes on Test Suite - Please Read
+1. Will need to run on new/fresh page and will need to refresh page after to play a new game
+2. If one test fails, all the following will fail. Solve errors in order they arrive.
+3. Do not click anything until tests finish.
+
+## Instructions to Play
+
+Welcome to mini Battleship! All the same fun of battleship packed into a smaller game, because bigger is not always better.
+      
+  1. Each Player gets 3 ships that are one square big. Player 1 starts by loading their ships onto their board. 
+  2. Player 2 then loads their board 
+  3. Player 1 then begins the game by clicking a spot to attack on Player 2's board
+  4. Player 2 attacks on Player 1's board 
+  5. Repeat until a victor appears!
--- battleship.js
+++ battleship.js
@@ -17,6 +17,11 @@ const model = {
   player2Keys: [],
   playerAlreadyWon: false,
   turn: 'player1',
+  alreadySunkTestBoolean1: false,
+  alreadySunkTestBoolean2: false,
+  alreadyMissTestBoolean1: false,
+  alreadyMissTestBoolean2: false,
+  winFromDeadTestBoolean: false,
 }
 
 const view = {
@@ -58,21 +63,24 @@ const controller = {
             }
           } else {
             if (!model.playerAlreadyWon) {
-              alert(`Congratulations Player 2! You won!`)
-              view.displayMessage(`Player 2 Won!`)
+              view.displayMessage(`Congratulations Player 2! You won!`)
+              view.displayMessage(`To start a new game click the "new game" button at the top of the page`)
               createVictor()
               model.playerAlreadyWon = true
             } else {
               view.displayMessage(`Player 2 won from the dead!`)
+              model.winFromDeadTestBoolean = true
             }
           }
           model.turn = 'player1'
           break;
         case 2:
-          alert('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          view.displayMessage('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          model.alreadySunkTestBoolean2 = true
           break;
         case 3:
-          alert('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          view.displayMessage('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          model.alreadyMissTestBoolean2 = true
           break;
         default:
           console.log('error unknown case: battleship.js line 62')
@@ -89,7 +97,7 @@ const controller = {
           view.displayMessage(`Player 1 has placed 1 ship!`)
         }
       } else {
-        alert('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
+        view.displayMessage('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
       }
       if (model.player1Ships === 3) {
         model.player1LoadedShips = true
@@ -98,9 +106,9 @@ const controller = {
       }
     }
   } else {
-    alert('It is not your turn! Even in stressful times of war, we must not cheat.')
+    view.displayMessage('It is not your turn! Even in stressful times of war, we must not cheat.')
   }
-    console.log('click on p1 tile: ',num)
+    console.log('PASS: click on p1 tile: ',num)
   },
   player2BoardKeyClick: num => {
     if ((model.turn === 'player2' && !model.player2LoadedShips) || (model.turn === 'player1' && model.player2LoadedShips)) {
@@ -126,21 +134,24 @@ const controller = {
             }
           } else {
             if (!model.playerAlreadyWon) {
-              alert(`Congratulations Player 1! You won!`)
-              view.displayMessage(`Player 1 Won!`)
+              view.displayMessage(`Congratulations Player 1! You won!`)
+              view.displayMessage(`To start a new game click the "new game" button at the top of the page`)
               createVictor()
               model.playerAlreadyWon = true
             } else {
               view.displayMessage(`Player 1 won from the dead!`)
+              model.winFromDeadTestBoolean = true
             }
           }
           model.turn = 'player2'
           break;
         case 2:
-          alert('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          view.displayMessage('Sunk: They\'re already dead! Have mercy and attack somewhere else!')
+          model.alreadySunkTestBoolean1 = true
           break;
         case 3:
-          alert('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          view.displayMessage('Already Taken: All ships in this war cannot move, rest assured that no one has moved here')
+          model.alreadyMissTestBoolean1 = true
           break;
         default:
           console.log('error unknown case: battleship.js line 62')
@@ -157,18 +168,18 @@ const controller = {
           view.displayMessage(`Player 2 has placed 1 ship!`)
         }
       } else {
-        alert('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
+        view.displayMessage('Tile Occupied: Due to complaints from ship captains, we are no longer allowed to stack ships on top of each other. Please place your ship in an empty tile.')
       }
       if (model.player2Ships === 3) {
         model.player2LoadedShips = true
         model.turn = 'player1'
-        view.displayMessage(`Player 2 has placed all ships, Player 1 start!`)
+        view.displayMessage(`Player 2 has placed all ships, Player 1 start by attacking Player 2's board!`)
       }
     }
   } else {
-    alert('It is not your turn! Even in stressful times of war, we must not cheat.')
+    view.displayMessage('It is not your turn! Even in stressful times of war, we must not cheat.')
   }
-  console.log('click on p2 tile: ', num)
+  console.log('PASS: click on p2 tile: ', num)
 }
 }
 
@@ -193,15 +204,222 @@ const createVictor = () => {
   const text = document.createTextNode("Victor")
   imgTextNode.appendChild(text)
   imgTextNode.setAttribute("id", "victor_title")
-  document.getElementById("victor_container").appendChild(imgNode)
-  document.getElementById("victor_container").appendChild(imgTextNode)
+  document.getElementById("instruction_text").appendChild(imgNode)
+  document.getElementById("instruction_text").appendChild(imgTextNode)
 }
 
-init = () => {
+const init = () => {
   bindBoardClickToKeys()
-  view.displayMessage('Hello! Please read instructions..')
+  view.displayMessage('Welcome! Please read instructions..')
   console.log('finish init render')
   return 'complete'
 }
 
 window.onload = init
+
+// Testing with Vanilla JS
+// Check Developer Console after running
+
+const runTestingSuite = () => {
+  const Board = [0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0]
+  //make sure all models load correctly at start
+  assertArraysEqual(model.player1Board, Board, 'Expect player1 empty board to render on start')
+  assertArraysEqual(model.player2Board, Board, 'Expect player2 empty board to render on start')
+  assertEquals(model.player1Ships, 0, 'Expect player1 to have 0 ships placed on start')
+  assertEquals(model.player2Ships, 0, 'Expect player2 to have 0 ships placed on start')
+  assertEquals(model.player1LoadedShips, false, 'Expect player1 to have not loaded any ships on start')
+  assertEquals(model.player2LoadedShips, false, 'Expect player2 to have not loaded any ships on start')
+  assertEquals(model.player1Keys.length, 25, 'Expect player1 to have all click binded keys on start')
+  assertEquals(model.player2Keys.length, 25, 'Expect player2 to have all click binded keys on start')
+  assertEquals(model.playerAlreadyWon, false, 'Expect playerAlreadyWon to be false')
+  assertEquals(model.turn, 'player1', 'Expect first turn to be player1')
+  //check if message function works
+  view.displayMessage('testing... Please check Developer Console for information')
+  assertEquals(document.getElementById("messages").childNodes.length, 3, 'Expect view.displayMessage to display messages')
+  //check place ships work player 1
+  controller.player1BoardKeyClick(0)
+  controller.player1BoardKeyClick(1)
+  controller.player1BoardKeyClick(2)
+  if (model.player1LoadedShips) {
+    console.log('PASS: Expect Player1 ship placements to work')
+  } else {
+    console.log('FAIL: Expect Player1 ship placements to work')
+  }
+  //check player turn works after player1 places ships
+  if (model.turn === "player2") {
+    console.log(`PASS: Expect turn to switch after player1 places ships`)
+  } else {
+    console.log(`FAIL: Expected turns to switch after player1 places ships`)
+  }
+  //check place ships work player 2
+  controller.player2BoardKeyClick(0)
+  controller.player2BoardKeyClick(1)
+  controller.player2BoardKeyClick(2)
+  if (model.player2LoadedShips) {
+    console.log('PASS: Expect Player2 ship placements to work')
+  } else {
+    console.log('FAIL: Expect Player2 ship placements to work')
+  }
+  //check player turn works after player2 places ships
+  if (model.turn === "player1") {
+    console.log(`PASS: Expect turn to switch after player2 places ships`)
+  } else {
+    console.log(`FAIL: Expected turns to switch after player2 places ships`)
+  }
+  //player 2 click to miss
+  controller.player2BoardKeyClick(5)
+  if (model.player2Board[5] === 3) {
+    console.log(`PASS: Expect Player2 Board click from empty to miss`)
+  } else {
+    console.log(`FAIL: Expected Player2 Board click on empty spot does not equal miss`)
+  }
+  //Check Turn Swap Works
+  if (model.turn === "player2") {
+    console.log(`PASS: Expect turn to switch after player1 miss fire`)
+  } else {
+    console.log(`FAIL: Expected turn to switch after player1 miss fire`)
+  }
+  //check if miss/click working properly for player 1
+  controller.player1BoardKeyClick(5)
+  if (model.player1Board[5] === 3) {
+    console.log(`PASS: Expect Player1 Board click from empty to miss`)
+  } else {
+    console.log(`FAIL: Expeceted Player1 Board click on empty spot to equal miss`)
+  }
+  //Check Turn Swap Works
+  if (model.turn === "player1") {
+    console.log(`PASS: Expect turn to switch after player2 miss fire`)
+  } else {
+    console.log(`FAIL: Expected turn to switch after player2 miss fire`)
+  }
+  //player1 hit
+  controller.player2BoardKeyClick(0)
+  if (model.player2Board[0] === 2) {
+    console.log('PASS: Expect Player2 Board click to go from empty to hit')
+  } else {
+    console.log('FAIL: Expected player2 Board click to go from empty to hit')
+  }
+  //Check Turn Swap Works
+  if (model.turn === "player2") {
+    console.log(`PASS: Expect turn to switch after player2 hit fire`)
+  } else {
+    console.log(`FAIL: Expected turn to switch after player2 hit fire`)
+  }
+  //player2 hit
+  controller.player1BoardKeyClick(0)
+  if (model.player1Board[0] === 2) {
+    console.log('PASS: Expect Player1 Board click to go from empty to hit')
+  } else {
+    console.log('PASS: Expected Player1 Board click to go from empty to hit')
+  }
+  //Check Turn Swap Works
+  if (model.turn === "player1") {
+    console.log(`PASS: Expect turn to switch after player1 miss fire`)
+  } else {
+    console.log(`FAIL: Expected turn to switch after Player1 miss fire`)
+  }
+  //Check already hit player 2 board
+  controller.player2BoardKeyClick(0)
+  if (model.alreadySunkTestBoolean1) {
+    console.log('PASS: Expect already sunk to work for player2 board')
+  } else {
+    console.log('FAIL: Expected already sunk to have work for player2 board')
+  }
+  // check turn still same player
+  if (model.turn === "player1") {
+    console.log('PASS: Expected turn to stay on Player1')
+  } else {
+    console.log('FAIL: Expected turn to still be Player1')
+  }
+  //check already miss player 2 board
+  controller.player2BoardKeyClick(5)
+  if (model.alreadyMissTestBoolean1) {
+    console.log('PASS: Expect already miss to work on player2 board')
+  } else {
+    console.log('FAIL: Expected already miss to work on player2 board')
+  }
+  //check turn still same player
+  if (model.turn === "player1") {
+    console.log('PASS: Expected turn to stay on Player1')
+  } else {
+    console.log('FAIL: Expected turn to still be player1')
+  }
+  model.turn = 'player2'
+  //check already hit player 1 board
+  controller.player1BoardKeyClick(0)
+  if (model.alreadySunkTestBoolean2) {
+    console.log('PASS: Expect already sunk test to work Player1 board')
+  } else {
+    console.log('FAIL: Expected already sunk test to work on Player1 board')
+  }
+  //check turn still same player
+  if (model.turn === "player2") {
+    console.log('PASS: Expected turn to stay on Player2')
+  } else {
+    console.log('FAIL: Expected turn to still be Player2')
+  }
+  // check already miss player 1 board
+  controller.player1BoardKeyClick(5)
+  if (model.alreadyMissTestBoolean2) {
+    console.log('PASS: Expect already miss test to work Player1 board')
+  } else {
+    console.log('FAIL: Expected already miss test to work on Player1 board')
+  }
+  // check turn still player2
+  if (model.turn === "player2") {
+    console.log('PASS: Expected turn to stay on Player2')
+  } else {
+    console.log('FAIL: Expected turn to still be Player2')
+  }
+  controller.player1BoardKeyClick(1)
+  model.turn = 'player2'
+  controller.player1BoardKeyClick(2)
+  // check if win works
+  if (model.playerAlreadyWon) {
+    console.log('PASS: Expect win to work')
+  } else {
+    console.log('FAIL: Expected win to work')
+  }
+  // check if win from dead works
+  controller.player2BoardKeyClick(1)
+  model.turn = 'player1'
+  controller.player2BoardKeyClick(2)
+  if (model.winFromDeadTestBoolean) {
+    console.log('PASS: Expect After death win to work')
+  } else {
+    console.log('FAIL: Expected After death win to work')
+  }
+  console.log('All Tests completed')
+  view.displayMessage('Testing complete, please check log for more information..')
+}
+
+// check if a = b
+const assertEquals = (a, b, string) => {
+if (a == b) {
+  console.log(`PASS: ${string}`)
+  return true
+} else {
+    console.log(`FAIL ${string}: Expected ${a} to equal ${b}`)
+    return false
+  }
+}
+
+// arrays have to be same order to pass
+const assertArraysEqual = (arr1, arr2, string) => {
+  if (arr1.length !== arr2.length) {
+    console.log(`FAIL ${string}: Expected ${arr1} to equal ${arr2}`)
+    return false
+  }
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`FAIL ${string}: Expected ${arr1} to equal ${arr2}`)
+      return false
+    }
+  }
+  console.log(`PASS ${string}`)
+  return true
+}
